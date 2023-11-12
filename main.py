ce0d83f434222dfe9ae71898c28af5be1cfb11a9
TomEijkelenkamp <66938518+TomEijkelenkamp@users.noreply.github.com>
2023-11-12 18:54:05 +0100

    loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from diffusers import DiffusionPipeline
 import torch
 import os
@@ -21,34 +22,38 @@ refiner.to("cuda")
 n_steps = 40
 high_noise_frac = 0.8
 
-# Settings
-prompt = "light and shadow configuration, complexity structure, illumination, artistic artstyle"
-number_of_images = 100
-folder_path = "generated_images/"
+object_list = ["cube", "sphere", "cylinder", "cone", "torus"]
+material_list = ["cardboard", "pvc", "plaster", "rubber", "stone", "cloth", "wood"]
 
-# Check if the folder already exists
-if not os.path.exists(folder_path):
-    # If it does not exist, create it
-    os.makedirs(folder_path)
+
+# Create directory if it doesn't exist
+folder_name = "images_{}".format(time())
+if not os.path.exists(folder_name):
+    os.makedirs(folder_name)
 
 # Create 100 images
 for index in range(100):
+    for object_type in object_list:
+        for material_type in material_list:
 
-    # Run both experts
-    image = base(
-        prompt=prompt,
-        num_inference_steps=n_steps,
-        denoising_end=high_noise_frac,
-        output_type="latent",
-    ).images
-    image = refiner(
-        prompt=prompt,
-        num_inference_steps=n_steps,
-        denoising_start=high_noise_frac,
-        image=image,
-    ).images[0]
+            prompt = f"even flat {material_type} diffuse ground surface, one solid {object_type} made out of an even flat {material_type} diffuse material"
 
-    file_name = "image_{}.png".format(index)
-    full_path = os.path.join(folder_path, file_name)
-    # Save image
-    image.save(full_path, format="PNG")
+            # Run both experts
+            image = base(
+                prompt=prompt,
+                num_inference_steps=n_steps,
+                denoising_end=high_noise_frac,
+                output_type="latent",
+            ).images
+            image = refiner(
+                prompt=prompt,
+                num_inference_steps=n_steps,
+                denoising_start=high_noise_frac,
+                image=image,
+            ).images[0]
+
+            file_name = "image_{}_{}_{}.png".format(object_type, material_type, index)
+            full_path = os.path.join(folder_name, file_name)
+            
+            # Save image
+            image.save(full_path, format="PNG")
